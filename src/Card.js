a73f12cde125e8730a22f8940e5ecbe48b5d93d9
John Chong <johnchong.99.17@gmail.com>
2023-05-30 14:13:25 -0500

    finish pt 1

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,28 @@
 import React from "react";
 
-function Card({ cardData }) {
-  const card = cardData.cards[0]
+/** Card renders single card pulled from deck
+ *
+ * Prop:
+ *  -cardData: result from API request {API result,...}
+ *  -idx: order in fetched cards array
+ *
+ *  App => DeckOfCards => Card
+ */
+function Card({ cardData, idx }) {
+  function generateRandDeg(ref) {
+    return Math.floor(Math.random() * ref);
+  }
+  const cardStyle = {
+    transform: `rotate(${generateRandDeg(360)}deg)`,
+    "z-index": idx,
+    position: "absolute",
+    left: `${500 + generateRandDeg(10)}px`,
+    top: `${100 + generateRandDeg(3)}px`
+  }
+  console.log("cardData",cardData)
+  const card = cardData.data.cards[0]
 
-  return (<div>
+  return (<div style={cardStyle}>
     <img src={card.image} alt={`${card.value} ${card.suit}`} />
   </div>);
 }
--- src/DeckOfCards.js
+++ src/DeckOfCards.js
@@ -3,11 +3,19 @@ import axios from "axios";
 import Card from "./Card";
 
 const BASE_URL = "https://deckofcardsapi.com/api/deck";
-
+/** DeckOfCards renders cardapp
+ *
+ *  State:
+ *  -card: contains data regarding displayed card {data, isLoading}
+ *  -deckId: contains data regarding deck used to generate card {data, isLoading}
+ *
+ *  App => DeckOfCards => Card
+ */
 function DeckOfCards() {
-  const [card, setCard] = useState({ data: null, isLoading: true });
+  const [cards, setCard] = useState([]);
   const [deckId, setDeckId] = useState({ data: "", isLoading: true });
 
+  /** sets new deckId only AFTER MOUNT */
   useEffect(function fetchNewDeckOnMount() {
     async function fetchNewDeck() {
       const newDeck = await axios.get(`${BASE_URL}/new/shuffle/`);
@@ -16,20 +24,26 @@ function DeckOfCards() {
     fetchNewDeck();
   }, []);
 
+  /** callback function to draw card from deck */
   async function fetchCard() {
+    if (cards.length === 52) {
+      console.log("you're done.")
+      return
+    }
     const newCard = await axios.get(`${BASE_URL}/${deckId.data}/draw/?count=1`);
-    setCard({ data: newCard.data, isLoading: false });
+    setCard([...cards, newCard]);
   }
 
   if (deckId.isLoading) return <i>Loading...</i>;
-  console.log("deckId", deckId);
 
   return (
     <div>
-      <button onClick={fetchCard}>Give me a card!</button>
-      {
-        card.data && <Card cardData={card.data} />
-      }
+      {cards.length < 52 && <button onClick={fetchCard}>Give me a card!</button>}
+      {cards.length < 52 && cards.map((card,idx) => (
+        <Card cardData={card} order={idx} />
+      ))}
+      {cards.length === 52 &&
+          <h1> No cards remaining! </h1>}
     </div>
   );
 }
